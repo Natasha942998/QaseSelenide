e5bd8ddfbd7edba9f73514ef4bdd68eacb1cb112
NatashaSotnikovaQA <147651793+Natasha942998@users.noreply.github.com>
2025-10-11 09:07:13 +0300

    Update LoginPage.java

--- src/test/java/page/LoginPage.java
+++ src/test/java/page/LoginPage.java
@@ -6,9 +6,6 @@ import org.openqa.selenium.By;
 import static com.codeborne.selenide.Selenide.*;
 
 public class LoginPage {
-    //private final By USER_INPUT = By.cssSelector("[placeholder='Work email]");
-    //private final String USER_CSS = "[placeholder='Work email]";
-    //private final SelenideElement emailInput = $x("//*[@placeholder='Work email']");
     private final SelenideElement emailInput = $("[placeholder = 'Work email']");
     private final SelenideElement passwordInput = $x("//*[@placeholder='Password]");
     private final SelenideElement submit = $x("//*[text()='Sign in']");
